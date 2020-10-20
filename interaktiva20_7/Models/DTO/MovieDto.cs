137b3877dffedcee4f12af833ca35f9c0872c30b
jesper magnusson <Jeppe.M@live.se>
2020-10-20 13:41:31 +0200

    Kompletterade med properties i "MovieDto"

--- interaktiva20_7/Models/DTO/MovieDto.cs
+++ interaktiva20_7/Models/DTO/MovieDto.cs
@@ -7,6 +7,10 @@ namespace interaktiva20_7.Models.DTO
 {
     public class MovieDto
     {
-
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string ImdbID { get; set; }
+        public string Type { get; set; }
+        public string Poster { get; set; }
     }
 }
