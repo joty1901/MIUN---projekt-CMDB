f76280b4f31d3f4d6a683507639048933553b979
jesper magnusson <Jeppe.M@live.se>
2020-10-22 12:34:38 +0200

    .

---
+++ interaktiva20_7/Data/CmdbRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace interaktiva20_7.Data
+{
+    public class CmdbRepository : ICmdbRepository
+    {
+
+        private string baseUrl;
+
+        public CmdbRepository()
+        {
+
+        }
+    }
+}
