4091485b369dfb8239a62c5e5ce22717a0552749
jesper magnusson <Jeppe.M@live.se>
2020-10-27 12:55:21 +0100

    Added "missing image" image.

--- interaktiva20_7/Models/DTO/MovieDto.cs
+++ interaktiva20_7/Models/DTO/MovieDto.cs
@@ -16,7 +16,7 @@ namespace interaktiva20_7.Models.DTO
         public string Writer { get; set; }
         public string Actors { get; set; }
         public string Plot { get; set; }
-        public string Poster { get; set; }
+        public string Poster { get; set; } = "../wwwroot/images/missingimage.jpg";
         public string ImdbID { get; set; }
         public int numberOfLikes { get; set; }
         public int numberOfDislikes { get; set; }
