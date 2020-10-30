aa7e71731ceb6e7c0f3a2bf8ef48c0ec8d82b516
jesper magnusson <Jeppe.M@live.se>
2020-10-30 15:37:47 +0100

    Fixed SearchBar

--- interaktiva20_7/wwwroot/js/searchHandler.js
+++ interaktiva20_7/wwwroot/js/searchHandler.js
@@ -3,10 +3,14 @@ var menuSearchResult = document.getElementById('menuSearchResult').style;
 menuSearchResult.display = 'inline-block';
 let searchString = '';
 
+
 document.querySelector('body').addEventListener('keyup', function updateSearchString1() {
     searchString = document.getElementById('search').value;
-    searchString1 = document.getElementById('menuSearchBar').value;
 })
+
+function clickMovie(value) {
+    window.location = "/details?id=" + value
+}
 
 document.getElementById('searchBtn').addEventListener('click', function search() {
     window.location = "/search?id=" + searchString
@@ -20,32 +24,32 @@ document.getElementById('menuSearchBtn').addEventListener('click', function sear
     }
 })
 
+    var list, li, i
+    list = document.getElementById('myList')
+li = list.getElementsByClassName('listItemsInSearch')
 
 document.getElementById('menuSearchBar').addEventListener('keyup', function checkIfEnter(event) {
-    let searchString = this.value
+    searchString = this.value
+    let filter = searchString.toUpperCase()
+    let counter = 0
+    //searchString1 = document.getElementById('menuSearchBar').value
     if (event.keyCode == 13) {
         window.location = "/search?id=" + searchString
     }
-    console.log(searchString1)
-    var list, li, i
-    filter = searchString.toUpperCase()
-    list = document.getElementById("myList")
-    li = list.getElementsByTagName('li')
-
     for (i = 0; i < li.length; i++) {
-        blockquote = li[i].getElementsByTagName('blockquote')[0];
-        txtValue = blockquote.textContent
-        console.log(txtValue)
-        console.log(searchString)
-        if (txtValue.toUpperCase().indexOf(filter) > -1 && txtValue.toUpperCase().includes(filter)) {
-            li[i].style.display = "block;"
-            console.log('ListItem[' + i + '] has been changed to inline-block')
+        blockquote = li[i].getElementsByClassName('movieTitle_test')[0].childNodes[0].nodeValue;
+
+
+
+        if (blockquote.toUpperCase().indexOf(filter) > -1 && counter < 5) {
+            li[i].style.display = "block !important;"
+            li[i].style.cssText = "display:block;"
+            counter++;
         } else {
             li[i].style.display = "none";
-            console.log('ListItem[' + i + '] has been changed to none')
         }
     }
-
+    list.style.display = 'block'
 
 
 
