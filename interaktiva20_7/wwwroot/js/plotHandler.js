3ae4b6e62059ea8f950196bab121f88870dd8666
Jonathan <joty1901@users.noreply.github.com>
2020-11-02 10:21:02 +0100

    - Skrivit JavaScript som gör att man kan välja mellan full- och short plot i detaljsidan

--- interaktiva20_7/wwwroot/js/plotHandler.js
+++ interaktiva20_7/wwwroot/js/plotHandler.js
@@ -1,12 +1,26 @@
 ﻿
 const fullPlot = document.getElementById('plot').textContent
-let shortPlot = fullPlot
+let shortPlot = truncate(fullPlot, 200)
+document.querySelector('#read-more').style.cursor = "pointer";
+let readMore = document.querySelector('#read-more')
 
 if (fullPlot.length > 200) {
-    document.querySelector('#see-more').style.display = 'block'
-    let output = truncate(shortPlot, 200)
-    document.getElementById('plot').textContent = output
+    document.querySelector('#read-more').style.display = 'block'
+    document.getElementById('plot').textContent = shortPlot + readMore
 }
+
+document.querySelector('#read-more').addEventListener('click', function () {
+    let visiblePlot = document.getElementById('plot').textContent
+
+    if (visiblePlot.length <= 202) {
+        readMore.textContent = ' Hide'
+        document.getElementById('plot').textContent = fullPlot
+    }
+    else {
+        readMore.textContent = ' Read more'
+        document.getElementById('plot').textContent = shortPlot
+    }
+})
 
 function truncate(str, n) {
     return (str.length > n) ? str.substr(0, n - 1) + '...' : str;
