045ae3f428d8b32633f9081dac6e27e7dcd6f1b6
Jonathan <joty1901@users.noreply.github.com>
2020-11-09 10:56:49 +0100

    - Ändrat lite CSS på startsidan - Gjort en metod för att hämta viewModel. - Fixat IF-sats som kollar om det tillkommit filmer i CMDB och avgör om vi ska göra nya anrop till OMDB eller läsa in en redan sparad list.

--- interaktiva20_7/Controllers/StartController.cs
+++ interaktiva20_7/Controllers/StartController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 using interaktiva20_7.Models.DTO;
 using System.Text.Encodings.Web;
+using interaktiva20_7.ViewModels;
 
 namespace interaktiva20_7.Controllers
 {
@@ -26,19 +27,31 @@ namespace interaktiva20_7.Controllers
         [HttpGet]
         public async Task<IActionResult> Index()
         {
+            List<MovieDto> cmdbMovies = await cmdbRepository.GetCmdbMovies();
 
             if (string.IsNullOrEmpty(HttpContext.Session.GetString("MovieList")))
             {
-                var viewModel = await cmdbRepository.GetMovieViewModel();
-                HttpContext.Session.SetString("MovieList", JsonConvert.SerializeObject(viewModel.movies));
-                viewModel.savedList = JsonConvert.DeserializeObject<List<MovieDto>>(HttpContext.Session.GetString("MovieList"));
+                var viewModel = await GetViewModel();
                 return View(viewModel);
             }
-            else { 
+            else if (!cmdbMovies.Count().Equals(JsonConvert.DeserializeObject<List<MovieDto>>(HttpContext.Session.GetString("MovieList")).Count()))
+            {
+                var viewModel = await GetViewModel();
+                return View(viewModel);
+            }
+            else
+            {
                 var viewModel = await cmdbRepository.GetMovieViewModelFromSession(JsonConvert.DeserializeObject<List<MovieDto>>(HttpContext.Session.GetString("MovieList")));
                 return View(viewModel);
             }
+        }
 
+        private async Task<MoviesViewModel> GetViewModel()
+        {
+            var viewModel = await cmdbRepository.GetMovieViewModel();
+            HttpContext.Session.SetString("MovieList", JsonConvert.SerializeObject(viewModel.movies));
+            viewModel.savedList = JsonConvert.DeserializeObject<List<MovieDto>>(HttpContext.Session.GetString("MovieList"));
+            return viewModel;
         }
     }
 }
